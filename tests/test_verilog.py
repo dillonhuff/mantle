2d2152bd2e2a7dd97f264db096ac349cc23e43d2
Lenny Truong <lenny@cs.stanford.edu>
2017-07-20 13:01:42 -0700

    Refactor tests to use factory methods

--- tests/test_verilog.py
+++ tests/test_verilog.py
@@ -2,56 +2,27 @@ from magma import *
 from magma.backend import verilog
 from mantle.verilog import *
 
-
-def test_and():
-    And1 = DefineAnd(1)
-    assert verilog.compile(And1) == """
-module And1 (input  I0, input  I1, output  O);
-assign O = I0 & I1
+def bits_binop_test_factory(name, op):
+    def _test():
+        circ = eval("Define{}(1)".format(name))
+        assert verilog.compile(circ) == """
+module {}1 (input  I0, input  I1, output  O);
+assign O = I0 {} I1
 endmodule
 
-""".lstrip()
-    And2 = DefineAnd(2)
-    assert verilog.compile(And2) == """
-module And2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 & I1
+""".format(name, op).lstrip()
+        circ = eval("Define{}(2)".format(name))
+        assert verilog.compile(circ) == """
+module {}2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
+assign O = I0 {} I1
 endmodule
 
-""".lstrip()
+""".format(name, op).lstrip()
+    return _test
 
-
-def test_or():
-    Or1 = DefineOr(1)
-    assert verilog.compile(Or1) == """
-module Or1 (input  I0, input  I1, output  O);
-assign O = I0 | I1
-endmodule
-
-""".lstrip()
-    Or2 = DefineOr(2)
-    assert verilog.compile(Or2) == """
-module Or2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 | I1
-endmodule
-
-""".lstrip()
-
-
-def test_xor():
-    Xor1 = DefineXor(1)
-    assert verilog.compile(Xor1) == """
-module Xor1 (input  I0, input  I1, output  O);
-assign O = I0 ^ I1
-endmodule
-
-""".lstrip()
-    Xor2 = DefineXor(2)
-    assert verilog.compile(Xor2) == """
-module Xor2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 ^ I1
-endmodule
-
-""".lstrip()
+test_and = bits_binop_test_factory("And", "&")
+test_or = bits_binop_test_factory("Or", "|")
+test_xor = bits_binop_test_factory("Xor", "^")
 
 
 def test_invert():
@@ -71,123 +42,56 @@ endmodule
 """.lstrip()
 
 
-def test_unsigned_add():
-    Add1 = DefineUnsignedAdd(1)
-    assert verilog.compile(Add1) == """
-module UnsignedAdd1 (input [0:0] I0, input [0:0] I1, output [0:0] O);
-assign O = I0 + I1
+def shift_factory(direction, op):
+    def _test():
+        circ = eval("DefineShift{}(2, 1)".format(direction))
+        assert verilog.compile(circ) == """
+module Shift{}2_1 (input [1:0] I, output [1:0] O);
+assign O = I {} 2'd1
 endmodule
 
-""".lstrip()
-    Add2 = DefineUnsignedAdd(2)
-    assert verilog.compile(Add2) == """
-module UnsignedAdd2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 + I1
+""".format(direction, op).lstrip()
+        circ = eval("DefineShift{}(4, 2)".format(direction))
+        assert verilog.compile(circ) == """
+module Shift{}4_2 (input [3:0] I, output [3:0] O);
+assign O = I {} 4'd2
 endmodule
 
-""".lstrip()
+""".format(direction, op).lstrip()
 
-
-def test_unsigned_sub():
-    Sub1 = DefineUnsignedSub(1)
-    assert verilog.compile(Sub1) == """
-module UnsignedSub1 (input [0:0] I0, input [0:0] I1, output [0:0] O);
-assign O = I0 - I1
+    def _test_dynamic():
+        circ = eval("DefineDynamicShift{}(2)".format(direction))
+        assert verilog.compile(circ) == """
+module DynamicShift{}2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
+assign O = I0 {} I1
 endmodule
 
-""".lstrip()
-    Sub2 = DefineUnsignedSub(2)
-    assert verilog.compile(Sub2) == """
-module UnsignedSub2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 - I1
+""".format(direction, op).lstrip()
+
+    return _test, _test_dynamic
+
+test_shift_left, test_dynamic_shift_left = shift_factory("Left", "<<")
+test_shift_right, test_dynamic_shift_right = shift_factory("Right", ">>")
+
+def binop_test_factory(name, op):
+    def _test():
+        circ = eval("Define{}(1)".format(name))
+        assert verilog.compile(circ) == """
+module {}1 (input [0:0] I0, input [0:0] I1, output [0:0] O);
+assign O = I0 {} I1
 endmodule
 
-""".lstrip()
-
-
-def test_unsigned_mul():
-    Mul1 = DefineUnsignedMul(1)
-    assert verilog.compile(Mul1) == """
-module UnsignedMul1 (input [0:0] I0, input [0:0] I1, output [0:0] O);
-assign O = I0 * I1
+""".format(name, op).lstrip()
+        circ = eval("Define{}(2)".format(name))
+        assert verilog.compile(circ) == """
+module {}2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
+assign O = I0 {} I1
 endmodule
 
-""".lstrip()
-    Mul2 = DefineUnsignedMul(2)
-    assert verilog.compile(Mul2) == """
-module UnsignedMul2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 * I1
-endmodule
+""".format(name, op).lstrip()
+    return _test
 
-""".lstrip()
-
-
-def test_unsigned_div():
-    Div1 = DefineUnsignedDiv(1)
-    assert verilog.compile(Div1) == """
-module UnsignedDiv1 (input [0:0] I0, input [0:0] I1, output [0:0] O);
-assign O = I0 / I1
-endmodule
-
-""".lstrip()
-    Div2 = DefineUnsignedDiv(2)
-    assert verilog.compile(Div2) == """
-module UnsignedDiv2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 / I1
-endmodule
-
-""".lstrip()
-
-
-def test_shift_left():
-    ShiftLeft2_1 = DefineShiftLeft(2, 1)
-    assert verilog.compile(ShiftLeft2_1) == """
-module ShiftLeft2_1 (input [1:0] I, output [1:0] O);
-assign O = I << 2'd1
-endmodule
-
-""".lstrip()
-    ShiftLeft4_2 = DefineShiftLeft(4, 2)
-    assert verilog.compile(ShiftLeft4_2) == """
-module ShiftLeft4_2 (input [3:0] I, output [3:0] O);
-assign O = I << 4'd2
-endmodule
-
-""".lstrip()
-
-
-def test_dynamic_shift_left():
-    DynamicShiftLeft2 = DefineDynamicShiftLeft(2)
-    assert verilog.compile(DynamicShiftLeft2) == """
-module DynamicShiftLeft2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 << I1
-endmodule
-
-""".lstrip()
-
-
-def test_shift_right():
-    ShiftRight2_1 = DefineShiftRight(2, 1)
-    assert verilog.compile(ShiftRight2_1) == """
-module ShiftRight2_1 (input [1:0] I, output [1:0] O);
-assign O = I >> 2'd1
-endmodule
-
-""".lstrip()
-    ShiftRight4_2 = DefineShiftRight(4, 2)
-    assert verilog.compile(ShiftRight4_2) == """
-module ShiftRight4_2 (input [3:0] I, output [3:0] O);
-assign O = I >> 4'd2
-endmodule
-
-""".lstrip()
-
-
-def test_dynamic_shift_right():
-    DynamicShiftRight2 = DefineDynamicShiftRight(2)
-    assert verilog.compile(DynamicShiftRight2) == """
-module DynamicShiftRight2 (input [1:0] I0, input [1:0] I1, output [1:0] O);
-assign O = I0 >> I1
-endmodule
-
-""".lstrip()
+test_unsigned_add = binop_test_factory("UnsignedAdd", "+")
+test_unsigned_sub = binop_test_factory("UnsignedSub", "-")
+test_unsigned_mul = binop_test_factory("UnsignedMul", "*")
+test_unsigned_div = binop_test_factory("UnsignedDiv", "/")
